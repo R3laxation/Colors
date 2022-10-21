aae5082368bae5188f2cd84735b49c710b6bf1d9
R3laxation <memories@bk.ru>
2022-10-21 15:32:21 +0600

    Created setRandomColors function

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const cols = document.querySelectorAll('.col');
 
-function setRandomColors() {
-    cols.forEach(col => {
-
-    })
-};
-
-function generateRandomColor(){
+function generateRandomColor() {
     const hexCodes = '0123456789ABCDEF';
     let color = '';
-    for(let i = 0; i <6; i++){
+    for (let i = 0; i < 6; i++) {
         color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
     }
     return '#' + color;
 };
+
+function setRandomColors() {
+    cols.forEach(col => {
+        col.style.background = generateRandomColor();
+    })
+};
+
+setRandomColors();
