318a017b57fd0ce36ab271db4a92208e87a7d78d
R3laxation <memories@bk.ru>
2022-10-21 15:30:39 +0600

    Created generateRandomColor function

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ function setRandomColors() {
     })
 };
 
+function generateRandomColor(){
+    const hexCodes = '0123456789ABCDEF';
+    let color = '';
+    for(let i = 0; i <6; i++){
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color;
+};
